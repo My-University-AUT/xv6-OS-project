ebe96d27ebfdd7bd020d226d08aac9a0ca13bf1f
alinowrouzii <alinowrouzii@gmail.com>
2021-10-09 22:26:29 +0330

    Add proc.c functions prototype to can be visible in sysproc.c

--- xv6-public/user.h
+++ xv6-public/user.h
@@ -24,6 +24,10 @@ char* sbrk(int);
 int sleep(int);
 int uptime(void);
 
+int getHelloWorld(void);
+int getProcCount(void);
+int getReadCount(void);
+
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
