5d034225df5bda743475cee948841ca974804d25
alinowrouzii <alinowrouzii@gmail.com>
2021-10-14 00:01:12 +0330

    Add fork system call to show its effect on procCount

--- xv6-public/getProcCountTest.c
+++ xv6-public/getProcCountTest.c
@@ -5,7 +5,20 @@
 int main(void)
 {
     int proc_cnt = getProcCount();
-    printf(1, "\nprocess count: %d\n", proc_cnt);
-    printf(1, "\nsuccessfull\n");
+    printf(1, "\nprocess count before fork: %d\n", proc_cnt);
+
+    int pid = fork();
+
+    if (pid > 0)
+    {
+        printf(1, "\nprocess count after fork: %d\n", getProcCount());
+
+        wait(); // Wait for a child process to exit and return its pid.
+                // also returns -1 if process has no children.
+        printf(1, "\nprocess count after child process is over: %d\n", getProcCount());
+
+        printf(1, "\nsuccessfull\n");
+    }
+
     exit();
 }
