320e2bf5fd623c081d8e38b834d82825465db1ee
alinowrouzii <alinowrouzii@gmail.com>
2021-10-14 16:31:52 +0330

    Add simulation like code

--- xv6-public/getProcCountTest.c
+++ xv6-public/getProcCountTest.c
@@ -9,16 +9,27 @@ int main(void)
 
     int pid = fork();
 
-    if (pid > 0)
+    // Error occured when creating new child process
+    if (pid < 0)
     {
-        printf(1, "\nprocess count after fork: %d\n", getProcCount());
-
-        wait(); // Wait for a child process to exit and return its pid.
-                // also returns -1 if process has no children.
-        printf(1, "\nprocess count after child process is over: %d\n", getProcCount());
-
-        printf(1, "\nsuccessfull\n");
+        printf(1, "Error occured when creating child process");
+        exit();
     }
+    // below else if block is executed just for child process
+    else if (pid == 0)
+    {
+        sleep(500);
+        exit();
+    }
+
+    // below code is executed just for parent process
+    printf(1, "\nprocess count after fork: %d\n\n", getProcCount());
+
+    wait(); // Wait for a child process to exit and return its pid.
+            // also returns -1 if process has no children.
+    printf(1, "process count after child process is over: %d\n\n", getProcCount());
+
+    printf(1, "successfull\n");
 
     exit();
 }
