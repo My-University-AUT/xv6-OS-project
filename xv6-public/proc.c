445e21efe3969e3d1078e5fd6b6697cb9eafbc94
Amir01 <iran.volleyball79@gmail.com>
2022-01-21 17:34:42 +0330

    Add Non-preemptive Priority Scheduling algorithm

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -421,26 +421,79 @@ void scheduler(void)
 
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
-    for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
-    {
-      if (p->state != RUNNABLE)
-        continue;
 
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
+    // Implement Non-Preemptive Priority Scheduling
+    if (c->policy == 2) { 
 
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
+      int current_max_priority = 6; // The highest priority is '1' and the lowest priority is '6'
+      struct proc *selected_p;
+      uint min_creationtime;
+      for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+      {
+        if (p->state != RUNNABLE) 
+          continue;
 
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
+        // In this section, we find the max priority in ptable
+        if (p->priority < current_max_priority) {
+          current_max_priority = p->priority;
+        }
+        min_creationtime = p->creationTime;
+      }
+      
+      for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+      {
+        if (p->state != RUNNABLE)
+          continue;
+
+        // This section, find the first process in the ptable that has the maximum priority
+        // and also, has created before any other processes with the same priority
+        if (p->priority == current_max_priority && p->creationTime < min_creationtime) {
+          selected_p = p;
+          min_creationtime = p->creationTime;
+        }
+      }
+
+      if (!(c->proc) && selected_p) {
+        // Switch to chosen process.  It is the process's job
+        // to release ptable.lock and then reacquire it
+        // before jumping back to us.
+        c->proc = selected_p;
+        switchuvm(selected_p);
+        selected_p->state = RUNNING;
+
+        swtch(&(c->scheduler), selected_p->context);
+        switchkvm();
+
+        // Process is done running for now.
+        // It should have changed its p->state before coming back.
+        c->proc = 0;
+      }
+
+      release(&ptable.lock);
+
+    } else {
+      for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+      {
+        if (p->state != RUNNABLE)
+          continue;
+
+
+        // Switch to chosen process.  It is the process's job
+        // to release ptable.lock and then reacquire it
+        // before jumping back to us.
+        c->proc = p;
+        switchuvm(p);
+        p->state = RUNNING;
+
+        swtch(&(c->scheduler), p->context);
+        switchkvm();
+
+        // Process is done running for now.
+        // It should have changed its p->state before coming back.
+        c->proc = 0;
+      }
+      release(&ptable.lock);
     }
-    release(&ptable.lock);
   }
 }
 
