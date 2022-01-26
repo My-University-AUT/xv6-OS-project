154939fe9930c07b918bca3ee4f08eb68f7e0128
alinowrouzii <alinowrouzii@gmail.com>
2022-01-27 00:47:54 +0330

    Final test

--- xv6-public/RR_test.c
+++ xv6-public/RR_test.c
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
             break;
         }
     }
-    // // sleep(5);
+    sleep(5);
     int pid = getpid();
 
     doSomeDummyWork(1000);
@@ -59,19 +59,35 @@ int main(int argc, char *argv[])
             waitWithPData((void *)&pdata_arr[i]);
             // wait();
         }
-
+        int TAT_average=0;
+        int CBT_average=0;
+        int WT_average=0;
         printf(1, "after all the children are done:\n");
         for ( i = 0; i < num_of_children; i++)
         {
+            int TAT = pdata_arr[i].runningTime +  pdata_arr[i].readyTime + pdata_arr[i].sleepingTime;
+            int WT =  pdata_arr[i].sleepingTime;
+            int CBT = pdata_arr[i].runningTime;
+            TAT_average+= TAT;
+            WT_average+= WT;
+            CBT_average+= CBT;
+            
             printf(1, "========================================\n");
             printf(1, "value of pid: %d | priority: %d\n", pdata_arr[i].pid, pdata_arr[i].priority);
             printf(1, "value of creation time: %d\n", pdata_arr[i].creationTime);
             printf(1, "value of termination time: %d\n", pdata_arr[i].terminationTime);
             printf(1,"-------------\n");
-            printf(1, "value of running time(CBT): %d\n", pdata_arr[i].runningTime);
-            printf(1, "value of waiting time: %d\n", pdata_arr[i].sleepingTime);
-            printf(1, "value of turnAroundTime(TAT): %d\n", pdata_arr[i].runningTime +  pdata_arr[i].readyTime + pdata_arr[i].sleepingTime);
+            printf(1, "value of running time(CBT): %d\n", CBT);
+            printf(1, "value of waiting time: %d\n", WT);
+            printf(1, "value of turnAroundTime(TAT): %d\n", TAT);
         }
+        TAT_average /= num_of_children;
+        CBT_average /= num_of_children;
+        WT_average /= num_of_children;
+        printf(1, "=========================\n");
+        printf(1, "TAT average: %d\n", TAT_average);
+        printf(1, "CBT average: %d\n", CBT_average);
+        printf(1, "waiting time average: %d\n", WT_average);
 
         exit();
     }
